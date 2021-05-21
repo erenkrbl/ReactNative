3d3542b7b6a059a39cab13871b9e28462495352a
Joseph Abraham <ekurubal24@gmail.com>
2021-05-21 19:05:43 +0300

    First Commit

---
+++ practiceProject/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { SafeAreaView, Text, View} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello New Project</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
