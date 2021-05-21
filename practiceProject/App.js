387e72fd5a18a784258843298b1f305507a79eee
Joseph Abraham <ekurubal24@gmail.com>
2021-05-21 20:06:43 +0300

    Added justifyContent, alignItems

--- practiceProject/App.js
+++ practiceProject/App.js
@@ -1,28 +1,29 @@
 
 import React from 'react';
-import { SafeAreaView, Text, View} from 'react-native';
+import { SafeAreaView, View, StyleSheet} from 'react-native';
 
 
 
 const App = () => {
   return (
-    <SafeAreaView style={{backgroundColor:'green', flex:1}}>
-      <View style ={{backgroundColor:'red', flex:1}}>
-        <Text>Wellcome</Text>
+    <SafeAreaView style={{flex:1}}>
+      <View style={styles.container}>
+        <View style={{width:40, height:40, backgroundColor:'red'}} />
+        <View style={{width:40, height:40, backgroundColor:'blue'}} />
+        <View style={{width:40, height:40, backgroundColor:'green'}} />
       </View>
-      <View style={{backgroundColor:'blue', flex:1}}>
-        <Text>Hello New Project</Text>
-      </View>
-      <View style={{backgroundColor:'yellow', flex:1}}>
-        <Text>Hello New Project</Text>
-        <Text>Hello New Project</Text>
-        <Text>Hello New Project</Text>
-        <Text>Hello New Project</Text>
-        <Text>Hello New Project</Text>
-        <Text>Hello New Project</Text>
-      </View>
+      <View style={{flex:1, backgroundColor: 'gray'}}/>
     </SafeAreaView>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor:'#e0e0e0',
+    justifyContent: 'space-around', //'space-between', 'space-evenly', 'flex-start',
+    alignItems: 'center', // alignItems: 'flex-start', 'flex-end','baseline',
+  },
+});
