b1cc3019cd73099e2ca7f1629f0a99761cf84269
Joseph Abraham <ekurubal24@gmail.com>
2021-05-21 19:23:02 +0300

    Flex structure

--- practiceProject/App.js
+++ practiceProject/App.js
@@ -6,8 +6,19 @@ import { SafeAreaView, Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={{backgroundColor:'green', flex:1}}>
+      <View style ={{backgroundColor:'red', flex:1}}>
+        <Text>Wellcome</Text>
+      </View>
+      <View style={{backgroundColor:'blue', flex:1}}>
+        <Text>Hello New Project</Text>
+      </View>
+      <View style={{backgroundColor:'yellow', flex:1}}>
+        <Text>Hello New Project</Text>
+        <Text>Hello New Project</Text>
+        <Text>Hello New Project</Text>
+        <Text>Hello New Project</Text>
+        <Text>Hello New Project</Text>
         <Text>Hello New Project</Text>
       </View>
     </SafeAreaView>
