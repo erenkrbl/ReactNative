fa22ad92014194e20836a2f132d03045c7229e18
Joseph Abraham <ekurubal24@gmail.com>
2021-05-23 22:04:14 +0300

    Data returned as map

--- practiceProject/App.js
+++ practiceProject/App.js
@@ -1,36 +1,36 @@
 
-import React from 'react';
-import { 
-  SafeAreaView, 
-  View, 
-  StyleSheet, 
-  Text, 
-} from 'react-native';
+// import React from 'react';
+// import { 
+//   SafeAreaView, 
+//   View, 
+//   StyleSheet, 
+//   Text, 
+// } from 'react-native';
 
 
 
-const App = () => {
+// const App = () => {
 
-  return (
-    <SafeAreaView style={{flex:1}}>
-      <View style={styles.container}>
-        <Text style={[styles.text, {color: 'rgb(170, 71, 126)'}]}>Clarusway</Text>
-        <Text style={styles.text}>Components</Text> 
-      </View>
-    </SafeAreaView>
-  );
-};
+//   return (
+//     <SafeAreaView style={{flex:1}}>
+//       <View style={styles.container}>
+//         <Text style={[styles.text, {color: 'rgb(170, 71, 126)'}]}>Clarusway</Text>
+//         <Text style={styles.text}>Components</Text> 
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
 
-export default App;
+// export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 40,
-    fontWeight: 'bold',
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex:1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     fontSize: 40,
+//     fontWeight: 'bold',
+//   }
+// });
--- practiceProject/src/Main.js
+++ practiceProject/src/Main.js
@@ -3,79 +3,59 @@ import React from 'react';
 import { SafeAreaView, View, Text} from 'react-native';
 import MyBanner from './components/MyBanner';
 
-
+const food_data = [
+  {
+    id:0,
+    title: "Soups",
+    color: "#ef5350",
+    desc: "Delicious dishes",
+    isActive:true,
+  },
+  {
+    id:1,
+    title: "Main Courses",
+    color: "#ffa000",
+    desc: "Hearty flavors",
+    isActive:false,
+  },
+  {
+    id:2,
+    title: "İntermediate meals",
+    color: "#64b5f6",
+    desc: "Delicious apparatus",
+    isActive:true,
+  },
+  {
+    id:3,
+    title: "Salads",
+    color: "#4caf50",
+    desc: "Refresh salads",
+    isActive:false,
+  },
+  {
+    id:4,
+    title: "Drinks",
+    color: "#ffee58",
+    desc: "Cold & Hot Drinks",
+    isActive:true,
+  },
+];
 
 const App = () => {
-
-  const food_data = [
-    {
-      id:0,
-      title: "Soups",
-      color: "#ef5350",
-      desc: "Delicious dishes",
-      isActive:false
-    },
-    {
-      id:1,
-      title: "Main Courses",
-      color: "#ffa000",
-      desc: "Hearty flavors",
-      isActive:false
-    },
-    {
-      id:2,
-      title: "İntermediate meals",
-      color: "#64b5f6",
-      desc: "Delicious apparatus",
-      isActive:true
-    },
-    {
-      id:3,
-      title: "Salads",
-      color: "#4caf50",
-      desc: "Refresh salads",
-      isActive:false
-    },
-    {
-      id:4,
-      title: "Drinks",
-      color: "#ffee58",
-      desc: "Cold & Hot Drinks",
-      isActive:false
-    },
-
-
-  ]
 
   return (
     <SafeAreaView style={{flex:1}}>
       <View>
-        <MyBanner 
-          title= "Soups"
-          desc= "Delicious dishes"
-          color= "#ef5350"
-          isActive={false}
-        />
-         <MyBanner 
-          title= "Main Courses"
-          desc= "Hearty flavors"
-          color= "#ffa000"
-          isActive={true}
-        />
-         <MyBanner 
-          title= "İntermediate meals"
-          desc= "Delicious apparatus"
-          color= "#64b5f6"
-          isActive={true}
-        />
-        
-         <MyBanner 
-          title= "Salads"
-          desc= "Refresh salads"
-          color= "#4caf50"
-          isActive={true}
-        />
-
+        {food_data.map((food) => {
+          return (
+            <MyBanner
+              title= {food.title}
+              desc= {food.desc}
+              color= {food.color}
+              isActive= {food.isActive} 
+            />
+          );
+        })}
       </View>
     </SafeAreaView>
   );
