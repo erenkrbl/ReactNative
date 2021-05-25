db92198a937a0522d596758ba263abe3b831f7af
Joseph Abraham <ekurubal24@gmail.com>
2021-05-25 20:46:30 +0300

    Input component completed

--- loginPage/src/Login.js
+++ loginPage/src/Login.js
@@ -5,11 +5,13 @@ import {Input, Button} from './components';
 const Login = () => {
     return (
         <SafeAreaView style={{flex: 1}}>
-            <View style={{flex: 1}}>
+            <View style={{flex: 1, backgroundColor: '#b2dfdb'}}>
                 <Image 
                     source={require('./assets/cart.png')}
                     style={styles.logoStyle}
                 />
+                <Input holder= "Enter your e-mail" />
+                <Input holder= "Enter your password" />
             </View>
         </SafeAreaView>
     )
--- loginPage/src/components/Input.js
+++ loginPage/src/components/Input.js
@@ -1,13 +1,26 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {View, Text, TextInput, StyleSheet} from "react-native";
 
-const Input = () => {
+const Input = ({holder}) => {
     return (
-        <View>
-
+        <View style={styles.container}>
+            <TextInput 
+                placeholder = {holder}
+            />
         </View>
        
     )
 }
 
 export  { Input };
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor: '#eceff1',
+        padding: 10,
+        margin:10,
+        borderRadius:10,
+        marginVertical: 20,
+
+    }
+})
