bbad4ee605c8e1c80d64c047cc75cc07718df428
Joseph Abraham <ekurubal24@gmail.com>
2021-05-26 16:26:48 +0300

    FlatList added

--- newsPage/src/News.js
+++ newsPage/src/News.js
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView
+  ScrollView,
+  FlatList
 } from 'react-native';
 
 const myArray = Array.from(Array(1500).keys())
@@ -12,14 +13,19 @@ const myArray = Array.from(Array(1500).keys())
 const News = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-        <ScrollView style={{flex: 1}}>
+        {/* <ScrollView style={{flex: 1}}>
             <View>
                 {
                     myArray.map(u =><Text>{u}</Text>)
                 }
                 <Text>News Page</Text>
             </View>
-        </ScrollView>
+        </ScrollView> */}
+
+        <FlatList 
+            data= {myArray}
+            renderItem= {(data) => <Text style={{fontSize: 30}}>{data.item}</Text> }
+        />
       
     </SafeAreaView>
   );
