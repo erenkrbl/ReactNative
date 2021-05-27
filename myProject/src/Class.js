594242e84fa1b7c9e192bf5161bbb88139510a81
Joseph Abraham <ekurubal24@gmail.com>
2021-05-27 12:46:41 +0300

    State definition with class

--- myProject/src/Class.js
+++ myProject/src/Class.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Button} from 'react-native';
 
 class App extends React.Component{
     constructor(props){
         super(props)
+        this.state= {
+            counter: 0
+        }
     }
     render () {
         return (
             <SafeAreaView>
                 <View>
-                    <Text style={{fontSize: 30, fontWeight: 'bold'}}>Hi. Full Stack Class</Text>
+                    <Text style={{fontSize: 200, fontWeight: 'bold', alignSelf: 'center'}}>{this.state.counter}</Text>
+                    <Button 
+                        title = "classUP!"
+                        onPress = {() => this.setState({counter: this.state.counter + 1})}
+                    />
                 </View>
             </SafeAreaView>
         )
