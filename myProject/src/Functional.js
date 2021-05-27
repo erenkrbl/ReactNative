a3f3b82d88486f9cdfcbb0e93a56cb5e37d48b32
Joseph Abraham <ekurubal24@gmail.com>
2021-05-27 20:50:28 +0300

    useEffect examples added

--- myProject/src/Functional.js
+++ myProject/src/Functional.js
@@ -4,20 +4,34 @@ import { SafeAreaView, View, Text, Button } from 'react-native';
 const App = (props) => {
 
     const [counter, setCounter] = useState(0);
+    const [myNumber, setMyNumber] = useState(0);
     const updateCounter = () => setCounter(counter + 1);
     
     useEffect(() => {
         console.log("useEffect")
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        console.log("useEffect counter")
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("useEffect myNumber")
+    }, [myNumber])
 
     console.log("Render");
     return (
         <SafeAreaView>
             <View>
-                <Text style={{fontSize:80, alignSelf: 'center'}}>Count: {counter}</Text>
+                <Text style={{fontSize:60, alignSelf: 'center'}}>Count: {counter}</Text>
                 <Button 
                     title= "UP!"
                     onPress= {updateCounter}
+                />
+                <Text style={{fontSize:60, alignSelf: 'center'}}>myNumber: {myNumber}</Text>
+                <Button 
+                    title= "Change Number!"
+                    onPress= {()=>setMyNumber(myNumber +1 )}
                 />
             </View>
         </SafeAreaView>
