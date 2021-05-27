4c7c3f3e03e800187b7c4c037d539d24d0176f09
Joseph Abraham <ekurubal24@gmail.com>
2021-05-27 12:38:13 +0300

    Usestate defined

--- myProject/src/Functional.js
+++ myProject/src/Functional.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { SafeAreaView, View, Text, Button } from 'react-native';
 
 const App = (props) => {
+
+    const [counter, setCounter] = useState(0);
+
+    const updateCounter = () => setCounter(counter + 1);
     return (
         <SafeAreaView>
             <View>
-                <Text>Hello Full Stack Function</Text>
+                <Text style={{fontSize:200, alignSelf: 'center'}}>{counter}</Text>
+                <Button 
+                    title= "UP!"
+                    onPress= {updateCounter}
+                />
             </View>
         </SafeAreaView>
     )
