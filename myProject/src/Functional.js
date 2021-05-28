6336dcf16f5f32f44acefc9226699b57c0e468da
Joseph Abraham <ekurubal24@gmail.com>
2021-05-28 20:49:31 +0300

    Login page examples completed

--- myProject/src/Functional.js
+++ myProject/src/Functional.js
@@ -1,23 +1,44 @@
 import React, {useState, useEffect} from 'react';
-import { SafeAreaView, View, Text, TextInput, StyleSheet, Button, Dimensions } from 'react-native';
+import { SafeAreaView, View, Text, TextInput, StyleSheet, Button, Dimensions, Alert } from 'react-native';
 
 const App = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = () => {
+        Alert.alert("Clarusway", `Email: ${email}, Password: ${password}`)
+    }
    
     return (
         <SafeAreaView style={{flex: 1}}>
             <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
                <View style={styles.input}>
-                   <TextInput 
+                   <TextInput
+                        value = {email} 
                         placeholder = "Enter your e-mail..."
+                        onChangeText = {(userText) => setEmail(userText)}
+                        keyboardType = "email-address"
                    />
                </View>
                <View style={styles.input}>
-                   <TextInput 
+                   <TextInput
+                        value = {password} 
                         placeholder = "Enter your password..."
+                        onChangeText = {(userPass) => setPassword(userPass)}
+                        secureTextEntry = {true}
+
                    />
                </View>
                <Button 
                     title= "Login"
+                    onPress = {login}
+               />
+               <Button 
+                    title= "Clean"
+                    onPress = {() => {
+                        setEmail("");
+                        setPassword("");
+                    }}
                />
             </View>
         </SafeAreaView>
