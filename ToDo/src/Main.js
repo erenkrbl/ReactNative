4d5e435a3ec7cf72d370743497dfe444b38b29b8
Joseph Abraham <ekurubal24@gmail.com>
2021-05-30 12:49:33 +0300

    Todo project started

---
+++ ToDo/src/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+/**
+ * #303e45
+ * #a7b6bd
+ * #4a636e
+ * #ff9b31
+ */
+
+const Main = () => {
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            <View style={{flex: 1}}>
+                <Text>TODO</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default Main;
