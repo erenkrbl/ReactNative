0983ca6b76b001075c3c105f07deaed784ba6dc4
Joseph Abraham <ekurubal24@gmail.com>
2021-06-01 19:42:14 +0300

    Passing useState value

--- ReactnativeNavigation/src/pages/First.js
+++ ReactnativeNavigation/src/pages/First.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import {SafeAreaView, View, Text, Button} from 'react-native';
+import React, { useState } from 'react';
+import {SafeAreaView, View, Text, Button, TextInput} from 'react-native';
 
 const number = 55;
 
 const First = (props) => {
+
+    const [userName, setUserName] = useState("");
     return (
         <SafeAreaView>
             <View>
                 <Text style={{fontSize: 40}}>First Page</Text>
-                <Text style={{fontSize: 40}}>{number}</Text>
+                {/* <Text style={{fontSize: 40}}>{number}</Text> */}
+                <Text style={{fontSize: 40}}>{userName}</Text>
+                <View style={{backgroundColor: '#e0e0e0', padding: 5, margin: 5, borderRadius:5}}>
+                    <TextInput
+                        value={userName}
+                        onChangeText={(text) => setUserName(text)}
+                        // onChangeText={(userName) => setUserName(userName)}
+                    />
+                </View>
                 <Button 
                     title= "Go"
                     onPress= {() => props.navigation.navigate('SecondPage', {
-                        myNumber : number
+                        // myNumber : number
+                        selectedValue : userName
                     })}
                 />
             </View>
--- ReactnativeNavigation/src/pages/Second.js
+++ ReactnativeNavigation/src/pages/Second.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {SafeAreaView, View, Text, Button} from 'react-native';
 
 const Second = (props) => {
-    const userNumber = props.route.params.myNumber;
+    // const userNumber = props.route.params.myNumber;
+
+    const userValue = props.route.params.selectedValue;
     return (
         <SafeAreaView>
             <View>
                 <Text style={{fontSize: 40}}>Second Page</Text>
-                <Text style={{fontSize: 40}}>Number: {userNumber}</Text>
+                {/* <Text style={{fontSize: 40}}>Number: {userNumber}</Text> */}
+                <Text style={{fontSize: 40}}>Value: {userValue}</Text>
                 <Button 
                     title= "Back"
                     onPress={() => props.navigation.goBack()}
