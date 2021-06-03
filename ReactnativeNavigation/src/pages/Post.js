ad78f5e6b6e645ef3af7b9bcf50c43dc580c29a2
Joseph Abraham <ekurubal24@gmail.com>
2021-06-03 18:31:53 +0300

    Completed project

--- ReactnativeNavigation/src/pages/Post.js
+++ ReactnativeNavigation/src/pages/Post.js
@@ -3,14 +3,22 @@ import { SafeAreaView, Text, View, Image, Dimensions  } from 'react-native';
 
 const Post = (props) =>{
 
-  
+  const postData = props.route.params.selectedPost;
 
     return (
       <SafeAreaView>
         <View>
-          <Text style={{fontSize:40}}>username</Text>
-          <Image/>
-          <Text style={{fontSize:40}}>desc</Text>
+          <Text style={{
+            margin: 10, 
+            fontSize: 20, 
+            fontWeight: 'bold' }}>{postData.userName}</Text>
+          <Image
+            source={{uri: postData.img}}
+            style={{
+              height: Dimensions.get('window').height / 2
+            }}
+          />
+          <Text style={{margin: 10}}>{postData.description}</Text>
   
         </View>
       </SafeAreaView>
