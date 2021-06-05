161457a03a230c83dc97e0e1590201c7e55e0b89
Joseph Abraham <ekurubal24@gmail.com>
2021-06-05 16:09:06 +0300

    then-catch

--- RESTful/src/Main.js
+++ RESTful/src/Main.js
@@ -1,11 +1,24 @@
+import axios from 'axios';
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Button } from 'react-native';
 
 const Main = (props) => {
+
+    const fetchData = () => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+
+    }
     return (
         <SafeAreaView>
             <View>
                 <Text>Main</Text>
+                <Button title="Fetch Data" onPress={fetchData}/>
             </View>
         </SafeAreaView>
     )
