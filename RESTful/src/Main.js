aeba2d4f6ba9f71d1cad1079faa21154028f796a
Joseph Abraham <ekurubal24@gmail.com>
2021-06-05 18:06:42 +0300

    Button added

--- RESTful/src/Main.js
+++ RESTful/src/Main.js
@@ -1,39 +1,15 @@
 import axios from 'axios';
-import React, { useState } from 'react';
-import { SafeAreaView, View, Text, Button, FlatList } from 'react-native';
+import React from 'react';
+import { SafeAreaView, View, Text, Tou } from 'react-native';
+import Button from './components/Button';
 
 const Main = (props) => {
-    const [userData, setUserData] = useState([]);
-
-    const fetchDataThen = () => {
-
-        axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(({data}) => {
-                console.log(data)
-                setUserData(data)
-                
-            })
-            .catch((error) => {
-                console.log(error)
-            })  
-
-    }
-
-    const fetchDataAwait = async () => {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-
-        setUserData(response.data)
-    }
+    
     return (
         <SafeAreaView>
             <View>
-                <Text>Main</Text>
-                <Button title="Fetch Data with Then" onPress={fetchDataThen}/>
-                <Button title="Fetch Data with Await" onPress={fetchDataAwait}/>
-
-                <FlatList 
-                    data={userData}
-                    renderItem={({item}) => <Text>{item.name}</Text>}
+                <Button
+                    title="Suggest New Restaurant"
                 />
             </View>
         </SafeAreaView>
