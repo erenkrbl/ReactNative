010b4e774c5047400eb6cf78129a6092b493e115
Joseph Abraham <ekurubal24@gmail.com>
2021-06-09 21:05:47 +0300

    Added async-await

--- cityYelpApp/src/pages/CityList.js
+++ cityYelpApp/src/pages/CityList.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import {SafeAreaView, View, Text, FlatList} from 'react-native';
 
 const CityList = (props) => {
+
+    const [cityList, setCityList] = useState([]);
+
+    //ASYNC-AWAİT
+
+    const fetchCityData = async () => {
+        const response = await axios.get("https://opentable.herokuapp.com/api/cities");
+        //setCityList(data.cities);
+        console.log(response)
+    }
+
+    useEffect(() => {
+        fetchCityData();
+    }, [])
+
+    const renderCities = ({item}) => <Text>{item}</Text>
+
     return (
         <SafeAreaView>
             <View>
-                <Text>CityList</Text>
+                <FlatList
+                    data={cityList}
+                    renderItem={renderCities}
+                />
             </View>
         </SafeAreaView>
     )
