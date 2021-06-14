bedb92e7d301b82c4b0ef12724fca0ebd21342d2
Joseph Abraham <ekurubal24@gmail.com>
2021-06-14 19:10:03 +0300

    Search option added

--- cityYelpApp/src/pages/CityList.js
+++ cityYelpApp/src/pages/CityList.js
@@ -4,7 +4,11 @@ import {SafeAreaView, View, Text, FlatList} from 'react-native';
 
 import { CityItem, SearchBar } from '../components'
 
+let originalList = []
+
 const CityList = (props) => {
+
+    // const [originalList, setOriginalList] = useState([]);
 
     const [cityList, setCityList] = useState([]);
 
@@ -13,6 +17,8 @@ const CityList = (props) => {
     const fetchCityData = async () => {
         const { data } = await axios.get('https://opentable.herokuapp.com/api/cities');
         setCityList(data.cities);
+        originalList = [...data.cities]
+        // setOriginalList(data.cities)
     }
 
     useEffect(() => {
@@ -23,13 +29,24 @@ const CityList = (props) => {
 
     const renderSeparator = () => <Viev style={{ borderWidth: 1, borderColor: '#e0e0e0' }} />
 
+    function searchCity(search){
+        const filteredCities = originalList.filter(city => {
+            const text = search.toUpperCase();
+            const cityName = city.toUpperCase();
+            return cityName.indexOf(text) > -1;
+        })
+
+        setCityList(filteredCities)
+
+    }
+
     return (
         <SafeAreaView>
             <View>
 
                 <SearchBar 
                     placeholder= 'Search for a City...'
-                    onSearch = {(value) => console.log(value)}
+                    onSearch = {(value) => searchCity(value)}
                 />
                 <FlatList
                     keyExtractor={(_, index) => index.toString()}
